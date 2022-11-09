e3c461c47025f5dadd7ce4c83b14677447f8cbd7
Tuan Tran <hometran@gmail.com>
2022-11-08 21:43:11 -0800

    modified settings

--- app/btre/settings.py
+++ app/btre/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
